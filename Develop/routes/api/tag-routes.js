ee2eb55867b300cb6533ef427298efb7d693dd29
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-03-23 13:27:58 -0700

    finished the put and post routes for tags

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -35,12 +35,37 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create({
+      tag_name: req.body.tag_name
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update
+      (req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with that id!" });
+      return;
+    }
+    const updatedTagData = await Tag.findByPk(req.params.id);
+    res.status(200).json(updatedTagData);
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
